939be18503780e389ee397d97b3422f7db7cb147
whitehatstudent143 <71756425+whitehatstudent143@users.noreply.github.com>
2020-09-26 00:14:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 var fixedRect, movingRect;
 
 function setup() {
-
   createCanvas(1200,800);
   fixedRect = createSprite(600, 400, 50, 80);
   fixedRect.shapeColor = "green";
@@ -15,18 +14,22 @@ function draw() {
   background(0,0,0);  
   movingRect.x = World.mouseX;
   movingRect.y = World.mouseY;
-
-  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2) {
-  
-fixedRect.shapeColor="red"
-movingRect.shapeColor="red"
-
+  console.log("data dada");
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
+      && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
   }
   else {
-    fixedRect.shapeColor = "green";
     movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
   }
-
-
+  
+  <script>
+var str = "GeeksforGeeks";
+console.log(str);
+</script>
   drawSprites();
 }
